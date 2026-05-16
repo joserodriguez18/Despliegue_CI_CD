bd7415a0b886e421c68beff458f4ab53e1a8f091
joserodriguez18 <jmra0226@gmail.com>
2026-05-15 19:41:32 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace despliegue_ci_cd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
